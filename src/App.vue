dcc164fa397c123e2186b3c3313cfde5497ced69
kate <kate20210906@gmail.com>
2022-05-08 15:00:01 +0800

    clean not file

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <nextTick/>
-    <goodJob/> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <alert/> -->
     <TimePicker/>
-    <!-- <homeLock/> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
-// import nextTick from "@/views/nextTick.vue";
-// import alert from "@/views/alertWord.vue";
-// import goodJob from "@/views/goodJob.vue";
 import TimePicker from "@/views/TimePicker.vue";
-// import homeLock from "@/views/homeLock.vue"
 export default {
   name: 'App',
   components: {
-    // HelloWorld
-    // nextTick,
-    // goodJob,
-    // alert
     TimePicker
-    // homeLock
   }
 }
 </script>
